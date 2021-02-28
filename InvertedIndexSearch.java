c9b79822a8f70680c245a10f8db3e82758d5eee8
Amir Hessam Bolandi <48315566+ahessamb@users.noreply.github.com>
2021-02-28 15:58:06 +0330

    final version of phase02

--- InvertedIndexSearch.java
+++ InvertedIndexSearch.java
@@ -16,18 +16,11 @@ public class InvertedIndexSearch {
 
     private HashMap<String, ArrayList<Entry>> indexMap = new HashMap<String, ArrayList<Entry>>();
     private HashSet<Entry> allDocs = new HashSet<Entry>();
-    // private HashMap<String, String> fileContents = new HashMap<String, String>();
-    private final Set<String> stopWords = new HashSet<>(Arrays.asList("ourselves", "hers", "between", "yourself", "but",
-            "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be",
-            "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s",
-            "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these",
-            "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should",
-            "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at",
-            "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then",
-            "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself",
-            "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t",
-            "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here",
-            "than"));
+    private static final String NORMAL_PATTERN = "\\s(\\w+)";
+    private static final String PLUS_PATTERN = "\\+(\\w+)";
+    private static final String MINUS_PATTERN = "-(\\w+)";
+    private static final String NOT_WORDS_PATTERN = "[^a-z0-9]";
+    private static final String SPACE_PATTERN = "\\s+";
 
     public InvertedIndexSearch(HashMap<String, String> fileContents) {
         for (String id : fileContents.keySet()) {
@@ -37,46 +30,25 @@ public class InvertedIndexSearch {
     }
 
     public Result search(String input) {
-        if (input.contains(" ") || input.contains("+") || input.contains("-")) {
-            return new Result(this.searchQuery(input), true);
-        } else {
-            return new Result(this.searchWord(input), false);
-        }
+        Boolean multiWords = input.contains(" ") || input.contains("+") || input.contains("-");
+        return new Result(this.searchQuery(input), multiWords);
     }
 
     private HashSet<Entry> searchQuery(String query) {
-        HashSet<Entry> normalSet = new HashSet<Entry>(allDocs), plusSet = new HashSet<Entry>(),
-                minusSet = new HashSet<Entry>();
-        for (String normalStr : this.seperatQuery(" " + query, "\\s(\\w+)"))
+        var normalSet = new HashSet<Entry>(allDocs);
+        var plusSet = new HashSet<Entry>();
+        var minusSet = new HashSet<Entry>();
+        for (String normalStr : this.seperatQuery(" " + query, NORMAL_PATTERN))
             normalSet.retainAll(searchWord(normalStr));
-        for (String plusStr : this.seperatQuery(query, "\\+(\\w+)"))
+        for (String plusStr : this.seperatQuery(query, PLUS_PATTERN))
             plusSet.addAll(searchWord(plusStr));
-        for (String minusStr : this.seperatQuery(query, "-(\\w+)"))
+        for (String minusStr : this.seperatQuery(query, MINUS_PATTERN))
             minusSet.addAll(searchWord(minusStr));
         return this.combineResults(normalSet, plusSet, minusSet);
     }
 
-    private HashSet<Entry> combineResults(HashSet<Entry> normalSet, HashSet<Entry> plusSet, HashSet<Entry> minusSet) {
-        // add normal set, Intersect plus set, Minus minus set'
-        HashSet<Entry> result = new HashSet<Entry>();
-        result.addAll(normalSet);
-        result.addAll(plusSet);
-        result.removeAll(minusSet);
-        return result;
-    }
-
-    private ArrayList<String> seperatQuery(String query, String regex) {
-        ArrayList<String> words = new ArrayList<String>();
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(query);
-        while (matcher.find()) {
-            words.add(matcher.group(1));
-        }
-        return words;
-    }
-
     private HashSet<Entry> searchWord(String word) {
-        HashSet<Entry> result = new HashSet<Entry>();
+        var result = new HashSet<Entry>();
         String word_s = "";
         if (word.charAt(word.length() - 1) == 's') {
             word_s = word.substring(0, word.length() - 1);
@@ -92,11 +64,30 @@ public class InvertedIndexSearch {
         return result;
     }
 
+    private HashSet<Entry> combineResults(HashSet<Entry> normalSet, HashSet<Entry> plusSet, HashSet<Entry> minusSet) {
+        // add normal set, Intersect plus set, Minus minus set'
+        var result = new HashSet<Entry>();
+        result.addAll(normalSet);
+        result.addAll(plusSet);
+        result.removeAll(minusSet);
+        return result;
+    }
+
+    private ArrayList<String> seperatQuery(String query, String regex) {
+        var words = new ArrayList<String>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(query);
+        while (matcher.find()) {
+            words.add(matcher.group(1));
+        }
+        return words;
+    }
+
     private void addWords(String id, String content) {
         String[] listOfWords = getListOfWords(content);
         for (int i = 0; i < listOfWords.length; i++) {
             String word = listOfWords[i];
-            if (stopWords.contains(word))
+            if (StopWordsClass.STOP_WORDS.contains(word))
                 continue;
             if (indexMap.containsKey(word)) {
                 indexMap.get(word).add(new Entry(id, i + 1));
@@ -107,8 +98,8 @@ public class InvertedIndexSearch {
     }
 
     private String[] getListOfWords(String content) {
-        content = content.toLowerCase().replaceAll("[^a-z0-9]", " ");
-        return content.split("\\s+");
+        content = content.toLowerCase().replaceAll(NOT_WORDS_PATTERN, " ");
+        return content.split(SPACE_PATTERN);
     }
 
 }
--- Main.java
+++ Main.java
@@ -5,20 +5,22 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        FileReader fileReader = new FileReader("Phase01/data/EnglishData");
-        HashMap<String, String> fileContents = fileReader.readContent();
-        InvertedIndexSearch invertedIndexSearch = new InvertedIndexSearch(fileContents);
+    private static final String DATA_PATH = "Phase01/data/EnglishData";
+    private static final String WELCOME_MASSAGE = "Enter search word or enter 'exit()' to end: ";
+    private static final String EXIT_COMMAND = "exit()";
 
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader(DATA_PATH);
+        var fileContents = fileReader.readContent();
+        var invertedIndexSearch = new InvertedIndexSearch(fileContents);
+        var scanner = new Scanner(System.in);
         String inputStr = "";
         while (true) {
-            System.out.println("Enter search word or enter 'exit()' to end: ");
+            System.out.println(WELCOME_MASSAGE);
             inputStr = scanner.nextLine().toLowerCase();
-            if (inputStr.equals("exit()"))
+            if (inputStr.equals(EXIT_COMMAND))
                 break;
             System.out.println(invertedIndexSearch.search(inputStr));
-
         }
     }
 
