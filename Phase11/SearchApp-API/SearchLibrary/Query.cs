bbcc7af86b35a72daa149654c5595fdbe9692f9e
amirhessam <amirhessamb123@yahoo.com>
2021-04-25 18:21:35 +0430

    Asp Api - Bulk Path has some errors

---
+++ Phase11/SearchApp-API/SearchLibrary/Query.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SearchLibrary
+{
+    public class Query
+    {
+        public const string NORMAL_PATTERN = @"\s(\w+)";
+        public const string PLUS_PATTERN = @"\+(\w+)";
+        public const string MINUS_PATTERN = @"-(\w+)";
+        
+
+        public List<string> Normals { get; }
+        public List<string> Pluses { get; }
+        public List<string> Minuses { get; }
+        public Query(string query){ 
+            Normals = FindPattern(" "+query , NORMAL_PATTERN);
+            Minuses = FindPattern(query , MINUS_PATTERN);
+            Pluses = FindPattern(query , PLUS_PATTERN);   
+        }
+        public Query(string normals ,string pluses,string minuses)
+        {
+            Normals = normals.Split(" ").ToList();
+            Minuses = minuses.Split(" ").ToList();
+            Pluses = pluses.Split(" ").ToList();   
+        }
+        
+        public static List<string> FindPattern(string query, string pattern)
+        {
+            var words = new List<string>();
+            Regex regex = new Regex(pattern);
+            MatchCollection mathces = regex.Matches(query);
+            foreach (Match match in mathces)
+                words.Add(match.Groups[1].Value);
+            return words;
+        }
+    }
+}
---
+++ Phase11/SearchApp-API/WebApi/SearchApi/SearchApi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SearchApi.Controllers;
+using SearchLibrary;
+
+namespace SearchApi
+{
+    public class SearchApi
+    {
+        private static SearchEngine searchEngine;
+        private const string ELASTIC_URI = "http://localhost:9200";
+        private const string INDEX_NAME = "documents";
+
+
+        public static string Initialize(bool isCreated)
+        {
+            try
+            {
+                searchEngine = new SearchEngine(INDEX_NAME, new Uri(ELASTIC_URI), isCreated);
+                return "initialized";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType()} \n {e.Message} \n {e.StackTrace}");
+            }
+
+            return null;
+        }
+
+        public static string PostNewData(string newDocumentPath)
+        {
+            try
+            {
+                searchEngine.PostDocuments(newDocumentPath);
+                return "documents are added";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType()} \n {e.Message} \n {e.StackTrace}");
+            }
+            return null;
+        }
+
+
+        public static List<string> GetQuery(string queryPhrase)
+        {
+            var query = new Query(queryPhrase);
+            var responseList = searchEngine.Search(query.Normals, query.Pluses, query.Minuses);
+            return responseList;
+        }
+        
+        public static List<string> GetQuery(string normals ,string pluses,string minuses)
+        {
+            var query = new Query(normals ,pluses,minuses);
+            var responseList = searchEngine.Search(query.Normals, query.Pluses, query.Minuses);
+            return responseList;
+        }
+    }
+}
